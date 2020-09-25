3b6e7f18200d811d7ec8f8bceb2320e4079de5c7
gustavo.santos97 <gustavoms97@hotmail.com>
2020-09-24 22:02:17 -0300

    feat: adiciona cors

--- src/infra/api/api.js
+++ src/infra/api/api.js
@@ -1,4 +1,5 @@
 const Express = require('express');
+const Cors = require('cors');
 
 exports.apiFactory = ({ ENV }) => {
   return {
@@ -6,6 +7,7 @@ exports.apiFactory = ({ ENV }) => {
       return new Promise((resolve, reject) => {
         const app = Express();
         app.use(Express.json({ limit: '1mb' }));
+        app.use(Cors());
         const server = app.listen(ENV.PORT, (serverError) => {
           if (serverError) {
             return reject(new Error('Não foi possivel inicializar o servidor'));
